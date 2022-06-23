2b56de92d1f8b20b1da514ebabcdb2e33d48ced7
Pierre Ossman <ossman@cendio.se>
2022-06-23 21:45:46 +0200

    Allow unregistering all signals at once
    Makes it easier and more robust to clean up when an object is destroyed.

--- tests/unit/signals.cxx
+++ tests/unit/signals.cxx
@@ -197,6 +197,7 @@ static void testDisconnectHelper()
 {
   Sender s;
   Receiver r;
+  Receiver r2;
   bool ok;
 
   printf("%s: ", __func__);
@@ -226,6 +227,18 @@ static void testDisconnectHelper()
     ok = true;
   }
   ASSERT_EQ(ok, true);
+
+  /* Disconnect all signals */
+  count = 0;
+  s.registerSignal("signal1");
+  s.registerSignal("signal2");
+  s.connectSignal("signal1", &r, &Receiver::genericHandler);
+  s.connectSignal("signal2", &r, &Receiver::genericHandler);
+  s.connectSignal("signal1", &r2, &Receiver::genericHandler);
+  s.disconnectSignals(&r);
+  s.emitSignal("signal1");
+  s.emitSignal("signal2");
+  ASSERT_EQ(count, 1);
 
   printf("OK\n");
 }
