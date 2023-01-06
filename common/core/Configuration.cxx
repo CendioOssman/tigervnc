5c92a1e4c743d8548a3cb3e9bc04422ed0c888c6
Pierre Ossman <ossman@cendio.se>
2023-01-06 15:46:29 +0100

    Don't change config value it it's the same
    Mainly avoids sending out the signal if there wasn't any actual change.

--- common/core/Configuration.cxx
+++ common/core/Configuration.cxx
@@ -304,6 +304,7 @@ bool BoolParameter::setParam() {
 
 void BoolParameter::setParam(bool b) {
   if (immutable) return;
+  if (value == b) return;
   value = b;
   vlog.debug("Set %s(Bool) to %s", getName(), getValueStr().c_str());
   emitSignal(&config);
@@ -350,6 +351,7 @@ IntParameter::setParam(const char* v) {
 bool
 IntParameter::setParam(int v) {
   if (immutable) return true;
+  if (value == v) return true;
   if (v < minValue || v > maxValue) {
     vlog.error("Int parameter %s: Invalid value '%d'", getName(), v);
     return false;
@@ -392,6 +394,7 @@ bool StringParameter::setParam(const char* v) {
   if (immutable) return true;
   if (!v)
     throw std::invalid_argument("setParam(<null>) not allowed");
+  if (value == v) return true;
   vlog.debug("Set %s(String) to %s", getName(), v);
   value = v;
   emitSignal(&config);
@@ -451,6 +454,7 @@ bool EnumParameter::setParam(const char* v)
     vlog.error("Enum parameter %s: Invalid value '%s'", getName(), v);
     return false;
   }
+  if (value == *iter) return true;
   vlog.debug("Set %s(Enum) to %s", getName(), iter->c_str());
   value = *iter;
   emitSignal(&config);
@@ -519,6 +523,7 @@ bool BinaryParameter::setParam(const char* v) {
 
 void BinaryParameter::setParam(const uint8_t* v, size_t len) {
   if (immutable) return; 
+  if ((length == len) && (memcmp(value, v, len) == 0)) return;
   vlog.debug("Set %s(Binary)", getName());
   delete [] value;
   value = nullptr;
@@ -605,6 +610,7 @@ bool ListParameter<ValueType>::setParam(const ListType& v)
     }
     vnorm.push_back(normaliseEntry(entry));
   }
+  if (value == vnorm) return true;
   value = vnorm;
   vlog.debug("set %s(List) to %s", getName(), getValueStr().c_str());
   emitSignal(&config);
