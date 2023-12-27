1d28a68aa0c473458c2e26819c9b3d46fa8511e1
Pierre Ossman <ossman@cendio.se>
2023-12-27 15:53:09 +0100

    Use signals for when a connection is ready to use

--- vncviewer/CConn.h
+++ vncviewer/CConn.h
@@ -57,8 +57,6 @@ protected:
   void getUserPasswd(bool secure, std::string *user,
                      std::string *password) override;
 
-  void initDone() override;
-
   void setExtendedDesktopSize(unsigned reason, unsigned result,
                               int w, int h,
                               const rfb::ScreenSet& layout) override;
@@ -81,6 +79,8 @@ protected:
   void setLEDState(unsigned int state) override;
 
 private:
+
+  void connectionReady();
 
   void resizeFramebuffer() override;
 
