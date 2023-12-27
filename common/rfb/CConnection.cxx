d3ad16432b861df1ba7ab4d058ac5acad3c53b2a
Pierre Ossman <ossman@cendio.se>
2023-12-27 15:53:09 +0100

    Use signals for when a connection is ready to use

--- common/rfb/CConnection.cxx
+++ common/rfb/CConnection.cxx
@@ -70,6 +70,8 @@ CConnection::CConnection()
     framebuffer(nullptr), decoder(this),
     hasRemoteClipboard(false), hasLocalClipboard(false)
 {
+  registerSignal("ready");
+
   registerSignal("clipboardrequest");
   registerSignal<bool>("clipboardannounce");
   registerSignal<const char*>("clipboarddata");
@@ -510,7 +512,8 @@ void CConnection::serverInit(int width, int height,
   state_ = RFBSTATE_NORMAL;
   vlog.debug("Initialisation done");
 
-  initDone();
+  emitSignal("ready");
+
   assert(framebuffer != nullptr);
   assert(framebuffer->width() == server.width());
   assert(framebuffer->height() == server.height());
@@ -692,10 +695,6 @@ void CConnection::handleClipboardProvide(uint32_t flags,
 
   // FIXME: Should probably verify that this data was actually requested
   emitSignal("clipboarddata", serverClipboard.c_str());
-}
-
-void CConnection::initDone()
-{
 }
 
 void CConnection::resizeFramebuffer()
