da95275381ab78b03b043769fa375dd052c6e186
Xavier BESSON <xavier.besson@qt.io>
2024-04-18 14:28:38 +0200

    Missing password file retrieving

--- vncviewer/vnccredential.cxx
+++ vncviewer/vnccredential.cxx
@@ -5,8 +5,10 @@
 #endif
 
 #include "rfb/Exception.h"
+#include "rfb/obfuscate.h"
 #include "appmanager.h"
 #include "i18n.h"
+#include "parameters.h"
 
 VNCCredential::VNCCredential()
  : QObject(nullptr)
@@ -19,19 +21,34 @@ VNCCredential::~VNCCredential()
 
 void VNCCredential::getUserPasswd(bool secure, std::string *user, std::string *password)
 {
+  const char *passwordFileName(::passwordFile);
   bool userNeeded = user != nullptr;
   bool passwordNeeded = password != nullptr;
   bool canceled = false;
   AppManager *manager = AppManager::instance();
   QString envUsername = QString(qgetenv("VNC_USERNAME"));
   QString envPassword = QString(qgetenv("VNC_PASSWORD"));
-  if (user && password && !envUsername.isEmpty() && !envPassword.isEmpty()) {
+  if (user && !envUsername.isEmpty() && !envPassword.isEmpty()) {
     user->assign(envUsername.toStdString());
     password->assign(envPassword.toStdString());
     return;
   }
-  if (password && !envPassword.isEmpty()) {
+  if (!user && !envPassword.isEmpty()) {
     password->assign(envPassword.toStdString());
+    return;
+  }
+  if (!user && passwordFileName[0]) {
+    std::vector<uint8_t> obfPwd(256);
+    FILE* fp;
+
+    fp = fopen(passwordFileName, "rb");
+    if (!fp)
+      throw rfb::Exception(_("Opening password file failed"));
+
+    obfPwd.resize(fread(obfPwd.data(), 1, obfPwd.size(), fp));
+    fclose(fp);
+
+    password->assign(rfb::deobfuscate(obfPwd.data(), obfPwd.size()));
     return;
   }
   connect(AppManager::instance(), &AppManager::authenticateRequested, this, [&](QString userText, QString passwordText) {
