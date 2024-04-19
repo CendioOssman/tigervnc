a586149cf30f7a5b901e576c5ecfd562b89e1af4
Xavier BESSON <xavier.besson@qt.io>
2024-04-19 12:46:54 +0200

    Mac dialogs

--- vncviewer/optionsdialog.cxx
+++ vncviewer/optionsdialog.cxx
@@ -63,9 +63,6 @@ OptionsDialog::OptionsDialog(bool staysOnTop, QWidget* parent)
 {
   setWindowTitle(_("VNC Viewer: Connection Options"));
   setWindowFlag(Qt::WindowStaysOnTopHint, staysOnTop);
-  setModal(true);
-  setWindowModality(Qt::ApplicationModal);
-
 
   QVBoxLayout* layout = new QVBoxLayout;
   layout->setContentsMargins(0,0,0,0);
