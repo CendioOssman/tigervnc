679ffd8162368307547469e9d5107316b5057f51
Xavier BESSON <xavier.besson@qt.io>
2024-05-22 11:20:42 +0200

    Proper mouse grabbing for X11

--- vncviewer/vncx11view.cxx
+++ vncviewer/vncx11view.cxx
@@ -22,7 +22,10 @@
 
 #include "X11KeyboardHandler.h"
 #include "rfb/LogWriter.h"
+#include "i18n.h"
 
+#include <X11/extensions/XInput2.h>
+#include <X11/extensions/XI2.h>
 #include <X11/XKBlib.h>
 
 static rfb::LogWriter vlog("QVNCX11View");
@@ -32,15 +35,116 @@ QVNCX11View::QVNCX11View(QWidget* parent, Qt::WindowFlags f)
 {
   keyboardHandler = new X11KeyboardHandler(this);
   initKeyboardHandler();
+
+#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
+  display = QX11Info::display();
+#else
+  display = qApp->nativeInterface<QNativeInterface::QX11Application>()->display();
+#endif
+}
+
+void QVNCX11View::grabPointer()
+{
+  // We also need to grab the pointer as some WMs like to grab buttons
+  // combined with modifies (e.g. Alt+Button0 in metacity).
+
+  // Having a button pressed prevents us from grabbing, we make
+  // a new attempt in fltkHandle()
+
+  XIEventMask *curmasks;
+  int num_masks;
+
+  int ret, ndevices;
+
+  XIDeviceInfo *devices, *device;
+  bool gotGrab;
+
+         // We grab for the same events as the window is currently interested in
+  curmasks = XIGetSelectedEvents(display, winId(), &num_masks);
+  if (curmasks == NULL) {
+    if (num_masks == -1)
+      vlog.error(_("Unable to get X Input 2 event mask for window 0x%08lx"), winId());
+    else
+      vlog.error(_("Window 0x%08lx has no X Input 2 event mask"), winId());
+
+    return;
+  }
+
+         // Our windows should only have a single mask, which allows us to
+         // simplify all the code handling the masks
+  if (num_masks > 1) {
+    vlog.error(_("Window 0x%08lx has more than one X Input 2 event mask"), winId());
+    return;
+  }
+
+  devices = XIQueryDevice(display, XIAllMasterDevices, &ndevices);
+
+         // Iterate through available devices to find those which
+         // provide pointer input, and attempt to grab all such devices.
+  gotGrab = false;
+  for (int i = 0; i < ndevices; i++) {
+    device = &devices[i];
+
+    if (device->use != XIMasterPointer)
+      continue;
+
+    curmasks[0].deviceid = device->deviceid;
+
+    ret = XIGrabDevice(display,
+                       device->deviceid,
+                       winId(),
+                       CurrentTime,
+                       None,
+                       XIGrabModeAsync,
+                       XIGrabModeAsync,
+                       True,
+                       &(curmasks[0]));
+
+    if (ret) {
+      if (ret == XIAlreadyGrabbed) {
+        continue;
+      } else {
+        vlog.error(_("Failure grabbing device %i"), device->deviceid);
+        continue;
+      }
+    }
+
+    gotGrab = true;
+  }
+
+  XIFreeDeviceInfo(devices);
+
+         // Did we not even grab a single device?
+  if (!gotGrab)
+    return;
+
+  QAbstractVNCView::grabPointer();
+}
+
+void QVNCX11View::ungrabPointer()
+{
+  int ndevices;
+  XIDeviceInfo *devices, *device;
+
+  devices = XIQueryDevice(display, XIAllMasterDevices, &ndevices);
+
+  // Release all devices, hoping they are the same as when we
+  // grabbed things
+  for (int i = 0; i < ndevices; i++) {
+    device = &devices[i];
+
+    if (device->use != XIMasterPointer)
+      continue;
+
+    XIUngrabDevice(display, device->deviceid, CurrentTime);
+  }
+
+  XIFreeDeviceInfo(devices);
+
+  QAbstractVNCView::ungrabPointer();
 }
 
 void QVNCX11View::bell()
 {
-
-#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
-  auto display = QX11Info::display();
-#else
-  auto display = qApp->nativeInterface<QNativeInterface::QX11Application>()->display();
-#endif
   XBell(display, 0 /* volume */);
 }
--- vncviewer/vncx11view.h
+++ vncviewer/vncx11view.h
@@ -3,6 +3,8 @@
 
 #include "abstractvncview.h"
 
+struct _XDisplay;
+
 class QVNCX11View : public QAbstractVNCView
 {
   Q_OBJECT
@@ -11,10 +13,15 @@ public:
   QVNCX11View(QWidget* parent = nullptr, Qt::WindowFlags f = Qt::Widget);
 
 public slots:
+  void grabPointer() override;
+  void ungrabPointer() override;
   void bell() override;
 
 signals:
   void message(const QString& msg, int timeout);
+
+private:
+  _XDisplay * display;
 };
 
 #endif // VNCX11VIEW_H
