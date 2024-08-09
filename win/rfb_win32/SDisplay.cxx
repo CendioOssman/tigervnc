42ab6777585a4cf966c3fccfdd96381866259044
Pierre Ossman <ossman@cendio.se>
2024-08-09 15:56:35 +0200

    Convert screen layout change request to signals

--- win/rfb_win32/SDisplay.cxx
+++ win/rfb_win32/SDisplay.cxx
@@ -122,6 +122,9 @@ void SDisplay::init(VNCServer* vs)
                         &SDisplay::handleClipboardAnnounce);
   server->connectSignal("clipboarddata", this,
                         &SDisplay::handleClipboardData);
+
+  server->connectSignal("layoutrequest", this,
+                        &SDisplay::layoutRequest);
 }
 
 void SDisplay::start()
@@ -185,8 +188,7 @@ void SDisplay::queryConnection(network::Socket* sock,
 }
 
 
-void SDisplay::setScreenLayout(int /*fb_width*/, int /*fb_height*/,
-                               const ScreenSet& /*layout*/)
+void SDisplay::layoutRequest(LayoutEvent)
 {
   assert(server != nullptr);
   server->rejectScreenLayout(rfb::resultProhibited);
