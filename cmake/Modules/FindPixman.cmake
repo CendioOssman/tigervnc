2b9317a0fb6fce4481ba80f4451ef419e317ac40
Pierre Ossman <ossman@cendio.se>
2024-09-25 15:23:40 +0200

    TEMP: Revert pixman changes

--- cmake/Modules/FindPixman.cmake
+++ cmake/Modules/FindPixman.cmake
@@ -1,12 +1,11 @@
-find_path(PIXMAN_INCLUDE_DIR NAMES pixman.h PATH_SUFFIXES pixman-1)
+find_package(PkgConfig)
 
-find_library(PIXMAN_LIBRARY NAMES pixman-1)
-
-find_package_handle_standard_args(pixman-1 DEFAULT_MSG PIXMAN_LIBRARY PIXMAN_INCLUDE_DIR)
-
-if(PIXMAN-1_FOUND)
-    set(PIXMAN_LIBRARIES ${PIXMAN_LIBRARY})
-    set(PIXMAN_INCLUDE_DIRS ${PIXMAN_INCLUDE_DIR})
+if (PKG_CONFIG_FOUND)
+	pkg_check_modules(PIXMAN pixman-1)
+else()
+	find_path(PIXMAN_INCLUDE_DIRS NAMES pixman.h PATH_SUFFIXES pixman-1)
+	find_library(PIXMAN_LIBRARIES NAMES pixman-1)
+	find_package_handle_standard_args(PIXMAN DEFAULT_MSG PIXMAN_LIBRARIES PIXMAN_INCLUDE_DIRS)
 endif()
 
 if(Pixman_FIND_REQUIRED AND NOT PIXMAN_FOUND)
