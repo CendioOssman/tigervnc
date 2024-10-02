14048c64ba1da5fa4f71a812e09dc3f0704b6d22
Pierre Ossman <ossman@cendio.se>
2024-10-02 13:23:35 +0200

    TEMP: Basic sync of KeyboardWin32.*

--- vncviewer/KeyboardWin32.h
+++ vncviewer/KeyboardWin32.h
@@ -1,35 +1,62 @@
-#ifndef WIN32KEYBOARDHANDLER_H
-#define WIN32KEYBOARDHANDLER_H
+/* Copyright 2011-2021 Pierre Ossman <ossman@cendio.se> for Cendio AB
+ * 
+ * This is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this software; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307,
+ * USA.
+ */
+
+#ifndef __KEYBOARDWIN32_H__
+#define __KEYBOARDWIN32_H__
 
 #include <rfb/Timer.h>
 
 #include "Keyboard.h"
 
-#include <windows.h>
-
-class KeyboardWin32 : public Keyboard
-{
+class KeyboardWin32 : public Keyboard {
 public:
   KeyboardWin32(KeyboardHandler* handler);
+  virtual ~KeyboardWin32();
 
   bool handleEvent(const char* eventType, void* message) override;
+
+  void reset() override;
 
   unsigned getLEDState() override;
   void setLEDState(unsigned state) override;
   void grabKeyboard() override;
   void ungrabKeyboard() override;
+
+protected:
+  uint32_t translateSystemKeyCode(int systemKeyCode);
+  uint32_t lookupVKeyMap(unsigned vkey, bool extended,
+                         const UINT map[][3], size_t size);
+  uint32_t translateVKey(unsigned vkey, bool extended);
+
+  bool hasAltGr();
   void handleAltGrTimeout(rfb::Timer*);
+  void resolveAltGrDetection(bool isAltGrSequence);
 
 private:
-  bool leftShiftDown, rightShiftDown;
+  int cachedHasAltGr;
+  HKL currentLayout;
 
-  bool altGrArmed = false;
+  bool altGrArmed;
   unsigned int altGrCtrlTime;
-  rfb::MethodTimer<KeyboardWin32> altGrCtrlTimer;
+  rfb::MethodTimer<KeyboardWin32> altGrTimer;
 
-  void resolveAltGrDetection(bool isAltGrSequence);
-  bool handleKeyDownEvent(UINT message, WPARAM wParam, LPARAM lParam);
-  bool handleKeyUpEvent(UINT message, WPARAM wParam, LPARAM lParam);
+  bool leftShiftDown;
+  bool rightShiftDown;
 };
 
-#endif // WIN32KEYBOARDHANDLER_H
+#endif
