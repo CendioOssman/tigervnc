e4b771098b4a1fb9bd9252e716b3ef12b1104ab6
Pierre Ossman <ossman@cendio.se>
2024-10-15 08:32:41 +0200

    Move translation setup to its own file
    Decrease some of the clutter in vncviewer.cxx.
    
    Note that this changes the console about message to always be in UTF-8.
    But non-UTF-8 systems are already poorly supported, so this mostly just
    aligns it with other console output.

--- vncviewer/vncviewer.cxx
+++ vncviewer/vncviewer.cxx
@@ -25,11 +25,9 @@
 #include <assert.h>
 #include <string.h>
 #include <stdio.h>
-#include <ctype.h>
 #include <stdlib.h>
 #include <errno.h>
 #include <signal.h>
-#include <locale.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/stat.h>
@@ -267,57 +265,6 @@ static void CleanupSignalHandler(int sig)
   exit(1);
 }
 
-static const char* getlocaledir()
-{
-#if defined(WIN32)
-  static char localebuf[PATH_MAX];
-  char *slash;
-
-  GetModuleFileName(nullptr, localebuf, sizeof(localebuf));
-
-  slash = strrchr(localebuf, '\\');
-  if (slash == nullptr)
-    return nullptr;
-
-  *slash = '\0';
-
-  if ((strlen(localebuf) + strlen("\\locale")) >= sizeof(localebuf))
-    return nullptr;
-
-  strcat(localebuf, "\\locale");
-
-  return localebuf;
-#elif defined(__APPLE__)
-  CFBundleRef bundle;
-  CFURLRef localeurl;
-  CFStringRef localestr;
-  Boolean ret;
-
-  static char localebuf[PATH_MAX];
-
-  bundle = CFBundleGetMainBundle();
-  if (bundle == nullptr)
-    return nullptr;
-
-  localeurl = CFBundleCopyResourceURL(bundle, CFSTR("locale"),
-                                      nullptr, nullptr);
-  if (localeurl == nullptr)
-    return nullptr;
-
-  localestr = CFURLCopyFileSystemPath(localeurl, kCFURLPOSIXPathStyle);
-
-  CFRelease(localeurl);
-
-  ret = CFStringGetCString(localestr, localebuf, sizeof(localebuf),
-                           kCFStringEncodingUTF8);
-  if (!ret)
-    return nullptr;
-
-  return localebuf;
-#else
-  return CMAKE_INSTALL_FULL_LOCALEDIR;
-#endif
-}
 static void init_fltk()
 {
   // Adjust look of FLTK
@@ -658,27 +605,11 @@ static int mktunnel()
 
 int main(int argc, char** argv)
 {
-  const char *localedir;
-
   argv0 = argv[0];
 
-  setlocale(LC_ALL, "");
+  i18n_init();
 
-  localedir = getlocaledir();
-  if (localedir == nullptr)
-    fprintf(stderr, "Failed to determine locale directory\n");
-  else
-    bindtextdomain(PACKAGE_NAME, localedir);
-  textdomain(PACKAGE_NAME);
-
-  // Write about text to console, still using normal locale codeset
   fprintf(stderr,"\n%s\n", about_text());
-
-  // Set gettext codeset to what our GUI toolkit uses. Since we are
-  // passing strings from strerror/gai_strerror to the GUI, these must
-  // be in GUI codeset as well.
-  bind_textdomain_codeset(PACKAGE_NAME, "UTF-8");
-  bind_textdomain_codeset("libc", "UTF-8");
 
   rfb::initStdIOLoggers();
 #ifdef WIN32
