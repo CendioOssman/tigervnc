d7f3848b3192b8d5ace507e9d5ba420302f28f51
Pierre Ossman <ossman@cendio.se>
2024-10-18 13:53:58 +0200

    TEMP: Remove old Qt Quick setup

--- vncviewer/vncviewer.cxx
+++ vncviewer/vncviewer.cxx
@@ -377,18 +377,6 @@ create_base_dirs()
 
 int main(int argc, char** argv)
 {
-  if (qEnvironmentVariableIsEmpty("QTGLESSTREAM_DISPLAY")) {
-    qputenv("QT_QPA_EGLFS_PHYSICAL_WIDTH", QByteArray("213"));
-    qputenv("QT_QPA_EGLFS_PHYSICAL_HEIGHT", QByteArray("120"));
-
-#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
-    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-#if QT_VERSION >= QT_VERSION_CHECK(5, 14, 0)
-    QGuiApplication::setHighDpiScaleFactorRoundingPolicy(Qt::HighDpiScaleFactorRoundingPolicy::PassThrough);
-#endif
-#endif
-  }
-
 #ifdef Q_OS_LINUX
   qputenv("QT_QPA_PLATFORM", "xcb");
 #endif
