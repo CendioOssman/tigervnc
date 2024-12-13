834703c763cacda6086a4682c2a69536da4a035c
Pierre Ossman <ossman@cendio.se>
2024-12-13 16:40:59 +0100

    Make connection user name always accessible

--- common/rfb/VNCServerST.cxx
+++ common/rfb/VNCServerST.cxx
@@ -809,8 +809,7 @@ void VNCServerST::connectTimeout()
   emitSignal("terminate");
 }
 
-void VNCServerST::queryConnection(VNCSConnectionST* client,
-                                  const char* userName)
+void VNCServerST::queryConnection(VNCSConnectionST* client)
 {
   // - Authentication succeeded - clear from blacklist
   blHosts->clearBlackmark(client->getSock()->getPeerAddress());
@@ -842,7 +841,7 @@ void VNCServerST::queryConnection(VNCSConnectionST* client,
     return;
   }
 
-  desktop->queryConnection(client->getSock(), userName);
+  desktop->queryConnection(client->getSock(), client->getUserName());
 }
 
 // -=- Internal methods
