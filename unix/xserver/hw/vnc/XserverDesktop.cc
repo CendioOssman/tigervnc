74123d12f054c87cff9613aa0d80ba0752ee9b82
Pierre Ossman <ossman@cendio.se>
2024-12-13 16:31:03 +0100

    Use empty string to indicate no user name
    It's easy to overlook that this can be NULL, so let's use a safer
    approach.

--- unix/xserver/hw/vnc/XserverDesktop.cc
+++ unix/xserver/hw/vnc/XserverDesktop.cc
@@ -209,7 +209,7 @@ void XserverDesktop::queryConnection(network::Socket* sock,
   }
 
   queryConnectAddress = sock->getPeerAddress();
-  if (!userName)
+  if (userName[0] == '\0')
     userName = "(anonymous)";
   queryConnectUsername = userName;
   queryConnectId = (uint32_t)(intptr_t)sock;
