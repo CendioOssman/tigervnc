26df78484f0e05793399095297c59629fcb04c39
Pierre Ossman <ossman@cendio.se>
2024-12-17 21:52:37 +0100

    Convert query connection to signals

--- win/rfb_win32/SDisplay.cxx
+++ win/rfb_win32/SDisplay.cxx
@@ -107,6 +107,9 @@ void SDisplay::init(VNCServer* vs)
   server->connectSignal("start", this, &SDisplay::start);
   server->connectSignal("stop", this, &SDisplay::stop);
 
+  server->connectSignal("queryconnection", this,
+                        &SDisplay::queryConnection);
+
   server->connectSignal("terminate", this,
                         [this]() { SetEvent(terminateEvent); });
 
@@ -172,7 +175,7 @@ void SDisplay::stop()
 }
 
 
-void SDisplay::queryConnection(rfb::SConnection* conn)
+void SDisplay::queryConnection(SConnection* conn)
 {
   assert(server != nullptr);
 
