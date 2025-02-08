0a744dc246459a21164e43f04ad2f37f5dc17ca7
Pierre Ossman <ossman@cendio.se>
2025-02-08 14:09:54 +0100

    Don't enable full screen until MapNotify
    Modern mutter (GNOME's window manager) has an internal race condition
    where changing state early is unreliable. Waiting for the MapNotify
    seems to get us past the unreliable zone.

--- vncviewer/DesktopWindow.cxx
+++ vncviewer/DesktopWindow.cxx
@@ -205,14 +205,6 @@ DesktopWindow::DesktopWindow(int w, int h, const char *name,
   // Adjust layout now that we're visible and know our final size
   repositionWidgets();
 
-  if (delayedFullscreen) {
-    // Hack: Fullscreen requests may be ignored, so we need a timeout for
-    // when we should stop waiting. We also really need to wait for the
-    // resize, which can come after the fullscreen event.
-    Fl::add_timeout(0.5, handleFullscreenTimeout, this);
-    fullscreen_on();
-  }
-
   // Show hint about menu key
   Fl::add_timeout(0.5, menuOverlay, this);
 
@@ -819,6 +811,19 @@ int DesktopWindow::fltkDispatch(int event, Fl_Window *win)
     case FL_UNFOCUS:
       if (fullscreenSystemKeys) {
         dw->ungrabKeyboard();
+      }
+      break;
+
+    case FL_SHOW:
+      // In this particular place, FL_SHOW means an actual MapNotify,
+      // which means we can continue enabling initial fullscreen.
+      if (dw->delayedFullscreen) {
+        // Hack: Fullscreen requests may be ignored, so we need a
+        // timeout for when we should stop waiting. We also really need
+        // to wait for the resize, which can come after the fullscreen
+        // event.
+        Fl::add_timeout(0.5, handleFullscreenTimeout, dw);
+        dw->fullscreen_on();
       }
       break;
 
